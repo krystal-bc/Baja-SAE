8c8609763233085b247a4a83a46bf1f53ae7c9e3
Krystal Bernal <krystalbc@sbcglobal.net>
2017-09-30 22:33:24 -0700

    Add files via upload

---
+++ MicroSD Breakout Board/SDCardInfo/SDCardInfo.ino
@@ -0,0 +1,96 @@
+/*
+  SD card test
+  created  28 Mar 2011 by Limor Fried
+  modified 9 Apr 2012 by Tom Igoe
+
+  Customized for Arduino Mega by Krystal Bernal
+  for CSULA Baja SAE 9/30/2017 
+
+  This example gets info about your SD card.
+  The circuit:
+  5V pin to the 5V pin on the Arduino
+  GND pin to the GND pin on the Arduino
+  CLK to pin 52
+  DO to pin 50
+  DI to pin 51
+  CS to pin 53  
+*/
+
+#include <SPI.h>
+#include <SD.h>
+
+Sd2Card card;
+SdVolume volume;
+SdFile root;
+
+const int CSPin = 53;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) {
+    ;
+  }
+
+  // test if the card is working
+  Serial.print("\nInitializing SD card...");
+  if (!card.init(SPI_HALF_SPEED, CSPin)) {
+    Serial.println("initialization failed. Things to check:");
+    Serial.println("* is a card inserted?");
+    Serial.println("* is your wiring correct?");
+    Serial.println("* did you change the CS Pin to match your shield or module?");
+    return;
+  } else {
+    Serial.println("Wiring is correct and a card is present.");
+  }
+
+  // print the type of card
+  Serial.print("\nCard type: ");
+  switch (card.type()) {
+    case SD_CARD_TYPE_SD1:
+      Serial.println("SD1");
+      break;
+    case SD_CARD_TYPE_SD2:
+      Serial.println("SD2");
+      break;
+    case SD_CARD_TYPE_SDHC:
+      Serial.println("SDHC");
+      break;
+    default:
+      Serial.println("Unknown");
+  }
+
+  // Now we will try to open the 'volume'/'partition' - it should be FAT16 or FAT32
+  if (!volume.init(card)) {
+    Serial.println("Could not find FAT16/FAT32 partition.\nMake sure you've formatted the card");
+    return;
+  }
+
+  // print the type and size of the first FAT-type volume
+  uint32_t volumesize;
+  Serial.print("\nVolume type is FAT");
+  Serial.println(volume.fatType(), DEC);
+  Serial.println();
+
+  volumesize = volume.blocksPerCluster();    // clusters are collections of blocks
+  volumesize *= volume.clusterCount();       // we'll have a lot of clusters
+  volumesize *= 512;                            // SD card blocks are always 512 bytes
+  Serial.print("Volume size (bytes): ");
+  Serial.println(volumesize);
+  Serial.print("Volume size (Kbytes): ");
+  volumesize /= 1024;
+  Serial.println(volumesize);
+  Serial.print("Volume size (Mbytes): ");
+  volumesize /= 1024;
+  Serial.println(volumesize);
+
+  Serial.println("\nFiles found on the card (name, date and size in bytes): ");
+  root.openRoot(volume);
+
+  // list all files in the card with date and size
+  root.ls(LS_R | LS_DATE | LS_SIZE);
+}
+
+
+void loop(void) {
+
+}
---
+++ MicroSD Breakout Board/SDFileTest/SDFileTest.ino
@@ -0,0 +1,60 @@
+#include <SPI.h>
+#include <SD.h>
+/*
+  5V pin to the 5V pin on the Arduino
+  GND pin to the GND pin on the Arduino
+  CLK to pin 52
+  DO to pin 50
+  DI to pin 51
+  CS to pin 53
+*/
+File myFile;
+const int CSPin = 53;
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect.
+  }
+  Serial.print("Serial monitor connected. Initializing SD card...");
+
+  if (SD.begin(CSPin)) {
+    Serial.println("initialization done.");
+    // open the file. note that only one file can be open at a time,
+    // so you have to close this one before opening another.
+    myFile = SD.open("bajaData.txt", FILE_WRITE);
+    // if the file opened okay, write to it:
+    if (myFile) {
+      Serial.print("Writing to bajaData.txt...");
+      myFile.println("testing 1, 2, 3.");
+      // close the file. if you don't close the file, no data will be saved.
+      myFile.close();
+      Serial.println("done.");
+      Serial.println("Opening bajaData.txt...");
+      // re-open the file for reading:
+      myFile = SD.open("bajaData.txt");
+      if (myFile) {
+        Serial.println("\tbajaData.txt:");
+        // read from the file until there's nothing else in it:
+        while (myFile.available()) {
+          Serial.write(myFile.read());
+        }
+        // close the file:
+        myFile.close();
+      } else {
+        // if the file didn't open, print an error:
+        Serial.println("error opening bajaData.txt");
+      }
+    } else {
+      // if the file didn't open, print an error:
+      Serial.println("error opening/creating bajaData.txt");
+    }
+  } else {
+    Serial.println("initialization failed.");
+  }
+}
+
+void loop() {
+  // nothing happens after setup
+}
