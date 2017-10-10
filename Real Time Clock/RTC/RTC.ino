24ce8d1764d9f32cccd09b93180d4a5defd5aeaf
Krystal Bernal <krystalbc@sbcglobal.net>
2017-10-10 15:17:54 -0700

    Add files via upload

---
+++ Real Time Clock/RTC/RTC.ino
@@ -0,0 +1,40 @@
+// Date and time functions using just software, based on millis() & timer
+
+#include <Arduino.h>
+#include <Wire.h>
+#include <RTClib.h>
+
+#if defined(ARDUINO_ARCH_SAMD)
+// for Zero, output on USB Serial console, remove line below if using programming port to program the Zero!
+  #define Serial SerialUSB
+#endif
+
+RTC_Millis rtc;
+
+void setup () {
+    Serial.begin(9600);
+    // following line sets the RTC to the date & time this sketch was compiled
+    rtc.begin(DateTime(F(__DATE__) , F(__TIME__)));
+    // This line sets the RTC with an explicit date & time, for example to set
+    // January 21, 2014 at 3am you would call:
+    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+}
+
+void loop () {
+    DateTime now = rtc.now();
+    
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print(' ');
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println();
+    
+    
+}
