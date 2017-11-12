9e9d4c997f9c6c1c9ba7b8325d1f2f828245974b
Krystal Bernal <krystalbc@sbcglobal.net>
2017-11-11 20:47:58 -0800

    Add files via upload

---
+++ Fan/Fan/Fan.ino
@@ -0,0 +1,30 @@
+/* Krystal Bernal
+ *  Last updated: 11/11/17
+ *  
+ *  This program sweeps a 12V CPU cooling fan
+ * 
+*/
+
+#define fanPIN 7
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(fanPIN, OUTPUT);
+  analogWrite(fanPIN, 0);
+
+}
+
+void loop() {
+  for(int i = 0; i<256; i++){
+    Serial.println(i);
+    analogWrite(fanPIN, i);
+    delay(50);
+  }
+  delay(3000);
+  for(int i = 256; i>0; i--){
+    Serial.println(i);
+    analogWrite(fanPIN, i);
+    delay(50);
+  }
+  delay(3000);
+}
