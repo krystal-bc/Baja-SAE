5a9e0d027b9fe9c6834dcd2b8cd8760dfeeea994
Krystal Bernal <krystalbc@gmail.com>
2018-03-29 23:49:58 -0700

    Add files via upload

--- Accelerometer/_2Accelerometers/_2Accelerometers.ino
+++ Accelerometer/_2Accelerometers/_2Accelerometers.ino
@@ -1,26 +1,24 @@
-// Basic demo for accelerometer readings from Adafruit LIS3DH
-//https://learn.adafruit.com/adafruit-lis3dh-triple-axis-accelerometer-breakout/wiring-and-test
+/*  Krystal Bernal
+ *  Last Updated: 3/29/2018
+ *  
+ *  This program reads data from two accelerometers
+ *  
+ *  https://learn.adafruit.com/adafruit-lis3dh-triple-axis-accelerometer-breakout/wiring-and-test
+ */
 
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_LIS3DH.h>
 #include <Adafruit_Sensor.h>
 
-// Used for software SPI
 #define CLK 13
 #define MISO 12
 #define MOSI 11
-// Used for hardware & software SPI
-#define Acc1_CS 10
-#define Acc2_CS 10
+#define Acc1_CS 5
+#define Acc2_CS 6
 
-// software SPI
 Adafruit_LIS3DH acc1 = Adafruit_LIS3DH(Acc1_CS, MOSI, MISO, CLK);
 Adafruit_LIS3DH acc2 = Adafruit_LIS3DH(Acc2_CS, MOSI, MISO, CLK);
-// hardware SPI
-//Adafruit_LIS3DH lis = Adafruit_LIS3DH(LIS3DH_CS);
-// I2C
-//Adafruit_LIS3DH lis = Adafruit_LIS3DH();
 
 #if defined(ARDUINO_ARCH_SAMD)
    #define Serial SerialUSB
@@ -29,11 +27,11 @@ Adafruit_LIS3DH acc2 = Adafruit_LIS3DH(Acc2_CS, MOSI, MISO, CLK);
 void setup(void) {
   Serial.begin(9600);
   
-  if (!acc1.begin(0x18)) {   // change this to 0x19 for alternative i2c address
+  if (!acc1.begin()) {
     Serial.println("Error initializing accelerometer 1");
     while (1);
   }
-  if (!acc2.begin(0x19)){
+  if (!acc2.begin()){
     Serial.println("Error initializing accelerometer 2");
     while (1);
   }
@@ -44,8 +42,7 @@ void setup(void) {
 
 void loop() {
   acc1.read();
-
-  /* Or....get a new sensor event, normalized */ 
+  
   sensors_event_t event1; 
   acc1.getEvent(&event1);
 
@@ -53,12 +50,12 @@ void loop() {
   acc1.getEvent(&event2);
   
   /* Display the results (acceleration is measured in m/s^2) */
-  Serial.print("\t\tX: "); Serial.print(event1.acceleration.x);
-  Serial.print(" \tY: "); Serial.print(event1.acceleration.y); 
-  Serial.print(" \tZ: "); Serial.print(event1.acceleration.z); 
-  Serial.print("\t\tX: "); Serial.print(event2.acceleration.x);
-  Serial.print(" \tY: "); Serial.print(event2.acceleration.y); 
-  Serial.print(" \tZ: "); Serial.print(event2.acceleration.z);
+  Serial.print("X: "); Serial.print(event1.acceleration.x);
+  Serial.print("   Y: "); Serial.print(event1.acceleration.y); 
+  Serial.print("   Z: "); Serial.print(event1.acceleration.z); 
+  Serial.print("      X: "); Serial.print(event2.acceleration.x);
+  Serial.print("   Y: "); Serial.print(event2.acceleration.y); 
+  Serial.print("   Z: "); Serial.print(event2.acceleration.z);
 
   Serial.println();
  
---
+++ Gyroscope/_2Gyros/_2Gyros.ino
@@ -0,0 +1,42 @@
+/*  Krystal Bernal
+ *  Last Updated: 3/29/18
+ *  This code uses SPI communication to read two L3GD20H 3-Axis Gyroscopes
+ */
+
+#include <Wire.h>
+#include <Adafruit_L3GD20.h>
+#include <Adafruit_L3GD20_U.h>
+
+#define CLK 13       //SCL
+#define DO 12        //SA0
+#define DI 11        //SDA
+#define gyro1_CS 8   //CS1
+#define gyro2_CS 7   //CS2
+
+Adafruit_L3GD20 gyro1 = Adafruit_L3GD20(gyro1_CS, DO, DI, CLK);
+Adafruit_L3GD20 gyro2 = Adafruit_L3GD20(gyro2_CS, DO, DI, CLK);
+
+void setup(){
+  Serial.begin(9600);
+  if (!gyro1.begin(250)) {
+    Serial.println("Error initializing gyro1.");
+    while (1);
+  }
+  if (!gyro2.begin(250)) {
+    Serial.println("Error initializing gyro2.");
+    while (1);
+  }
+}
+
+void loop(){
+  gyro1.read();
+  Serial.print("X: "); Serial.print((int)gyro1.data.x);   Serial.print(" ");
+  Serial.print("\tY: "); Serial.print((int)gyro1.data.y);   Serial.print(" ");
+  Serial.print("\tZ: "); Serial.println((int)gyro1.data.z); Serial.print(" ");
+
+  gyro2.read();
+  Serial.print("X: "); Serial.print((int)gyro2.data.x);   Serial.print(" ");
+  Serial.print("\tY: "); Serial.print((int)gyro2.data.y);   Serial.print(" ");
+  Serial.print("\tZ: "); Serial.println((int)gyro2.data.z); Serial.print(" ");
+  delay(10);
+}
