90f6eb637ea28ab88b5575d5bfa514321ac25568
Krystal Bernal <krystalbc@gmail.com>
2018-07-31 18:00:41 -0700

    Add files via upload

---
+++ Hall Effect Sensor/Hall Effect Sensor/HE_RPM/HE_RPM.ino
@@ -0,0 +1,26 @@
+#define HE_Pin A0
+
+boolean detect = false;
+boolean lastDetect = false;
+
+int currentTime = 0;
+int lastTime = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(HE_Pin, INPUT);
+}
+
+void loop() {
+  int HE_Raw = analogRead(HE_Pin);
+  if(HE_Raw > 750){
+    detect = true;
+  }else{
+    detect = false;
+  }
+  if(detect != lastDetect){
+    Serial.println(millis());
+  }
+  detect = lastDetect;
+
+}
